14cdd0dede002c054b67481ef4662fca824048f3
Krzysztof Adamek <kadamek@alx.pl>
2022-11-05 12:35:27 +0100

    zadanie_2

--- dzien_01/zadanie_2.cpp
+++ dzien_01/zadanie_2.cpp
@@ -5,3 +5,19 @@
 // 16 57 5
 // Powinno zostać wyświetlone:
 // 17 2
+#include <iostream>
+
+int main() {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    std::cin >> a >> b >> c;
+    std::cout << "Obecnie jest godzina " << a << ":" << b << "\n";
+    
+    int suma_minut = b + c;
+    int nowe_minuty = suma_minut % 60;
+    int nowe_godziny = a + suma_minut / 60;
+    nowe_godziny = nowe_godziny % 24;
+    
+    std::cout << "Po uplynieciu " << c << " minut bedzie godzina " << nowe_godziny << ":" << nowe_minuty << "\n";
+}
