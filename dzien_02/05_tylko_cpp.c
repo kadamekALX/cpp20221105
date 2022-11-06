9b55dd88678e3875ce514a83bac104320b891afe
Krzysztof Adamek <kadamek@alx.pl>
2022-11-06 15:29:09 +0100

    C vs C++

---
+++ dzien_02/05_tylko_cpp.c
@@ -0,0 +1,7 @@
+#include <iostream>
+
+//ten program skompiluje się tylko w C++, bo korzysta z iostream, które pochodzi z biblioteki standardowej C++
+
+int main() {
+    std::cout << "Hello\n";
+}
---
+++ dzien_02/06_c_lub_cpp.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Podaj liczbe: ");
+    int x;
+    scanf("%d", &x);
+    printf("Twoja liczba to %d\n", x);
+}
