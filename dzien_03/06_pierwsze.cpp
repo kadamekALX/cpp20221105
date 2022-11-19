388a654de1af0cd3d8ad530731d7e24ab3b724be
Krzysztof Adamek <kadamek@alx.pl>
2022-11-19 15:12:16 +0100

    Liczby pierwsze v1

---
+++ dzien_03/06_pierwsze.cpp
@@ -0,0 +1,38 @@
+// Napisz funkcję czy_pierwsza(), która sprawdzi, czy podany argument jest liczbą pierwszą.
+
+#include <iostream>
+
+bool czy_podzielne(int n, int k) {
+    return n % k == 0;
+}
+
+int suma_dzielnikow(int x) {
+    int suma = 0;
+    for (int i = 1; i < x; i += 1) {
+        if (x % i == 0)
+            suma += i;
+    }
+    return suma;
+}
+
+bool czy_pierwsza(int n) {
+//     return suma_dzielnikow(n) == 1;
+    if (n <= 1)
+        return false;
+    
+    for (int i = 2; i < n; i += 1) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    if (czy_pierwsza(n)) {
+        std::cout << "Pierwsza\n";
+    }
+}
+
+// 1000000007
