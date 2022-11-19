2fd6ed94611f1551fe0946323ba4ca8dca07e682
Krzysztof Adamek <kadamek@alx.pl>
2022-11-19 10:04:46 +0100

    Hopsasa v3

--- dzien_03/00_praca_domowa_5.cpp
+++ dzien_03/00_praca_domowa_5.cpp
@@ -24,15 +24,21 @@
 
 int main() {
     for (int i = 1; i <= 100; i += 1) {
+        bool czy_cos_wypisalismy = false;
         if (i % 3 == 0) {
             std::cout << "Hopsasa";
+            czy_cos_wypisalismy = true;
         }
-        
         if (i % 5 == 0) {
             std::cout << "Tralala";
+            czy_cos_wypisalismy = true;
+        }
+        if (i % 7 == 0) {
+            std::cout << "Bum";
+            czy_cos_wypisalismy = true;
         }
         
-        if (i % 3 != 0 && i % 5 != 0) {
+        if (!czy_cos_wypisalismy) /* czy_cos_wypisalismy == false*/ {
             std::cout << i;
         }
         
