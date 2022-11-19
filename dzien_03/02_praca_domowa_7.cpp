baeab1060d86e5c653e360dfef4324a804d0508c
Krzysztof Adamek <kadamek@alx.pl>
2022-11-19 10:50:10 +0100

    Szachownica v3

--- dzien_03/02_praca_domowa_7.cpp
+++ dzien_03/02_praca_domowa_7.cpp
@@ -12,7 +12,10 @@ int main() {
     std::cin >> n;
     for (int i = 0; i < n; i += 1) {
         for (int j = 0; j < n; j += 1) {
-            std::cout << (i + j) % 2;
+            if ((i + j) % 2 == 0)
+                std::cout << "X";
+            else
+                std::cout << "O";
         }
         std::cout << "\n";
     }
