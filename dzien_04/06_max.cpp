367acf605de744a5a17f1a036f52d68e3c6975a1
Krzysztof Adamek <kadamek@alx.pl>
2022-11-20 13:09:31 +0100

    Max

---
+++ dzien_04/06_max.cpp
@@ -0,0 +1,17 @@
+//Napisz funkcję, która przyjmie tablicę intów (i jej rozmiar) i zwróci największy element z tej tablicy
+
+#include <iostream>
+
+int max(int* tab, int rozmiar) {
+    int maximum = tab[0];
+    for (int i = 1; i < rozmiar; i += 1) {
+        if (tab[i] > maximum)
+            maximum = tab[i];
+    }
+    return maximum;
+}
+
+int main() {
+    int tab[5] = {4, 6, 8, 3, 5};
+    std::cout << "Najwiekszy element: " << max(tab, 5) << "\n";
+}
