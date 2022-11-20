dc40f5e8c3bc0be06dbb3bd013a601afc1dd4ba2
Krzysztof Adamek <kadamek@alx.pl>
2022-11-20 13:10:43 +0100

    Max + cin

--- dzien_04/06_max.cpp
+++ dzien_04/06_max.cpp
@@ -13,5 +13,7 @@ int max(int* tab, int rozmiar) {
 
 int main() {
     int tab[5] = {4, 6, 8, 3, 5};
+    for (int i = 0; i < 5; i += 1)
+        std::cin >> tab[i];
     std::cout << "Najwiekszy element: " << max(tab, 5) << "\n";
 }
