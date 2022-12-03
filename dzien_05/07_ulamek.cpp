ef100c646437dbc8ecc12ab5ff4c4d251bda8720
Krzysztof Adamek <kadamek@alx.pl>
2022-12-03 16:53:44 +0100

    Ulamek - zadanie 1

---
+++ dzien_05/07_ulamek.cpp
@@ -0,0 +1,21 @@
+// Stworz strukturę Ulamek (licznik i mianownik) oraz napisz funkcje:
+//     - void wypisz(const Ulamek& u)
+//     - Ulamek stworz(int a, int b) - tworzy Ulamek o liczniku a i mianowniku b
+//     - Ulamek mnoz(const Ulamek& a, const Ulamek& b) - zwraca wynik możenia Ulamkow a i b
+#include <iostream>
+
+void wypisz(const Ulamek& u) {
+    
+}
+
+Ulamek stworz(int a, int b) {
+    
+}
+
+Ulamek mnoz(const Ulamek& a, const Ulamek& b) {
+    
+}
+
+int main() {
+    
+}
