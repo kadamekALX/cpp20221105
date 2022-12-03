3431cbc1f3ad535d7aca33b2b1bc0c84a1ed2e23
Krzysztof Adamek <kadamek@alx.pl>
2022-12-03 17:20:32 +0100

    Liczby pierwsze - O(logn)

--- dzien_03/06_pierwsze.cpp
+++ dzien_03/06_pierwsze.cpp
@@ -23,8 +23,8 @@ bool czy_pierwsza(int n) {
         return true;
     if (n % 2 == 0)
         return false;
-    
-    for (int i = 3; i < n; i += 2) {
+
+    for (int i = 3; i * i <= n; i += 2) {
         if (n % i == 0)
             return false;
     }
