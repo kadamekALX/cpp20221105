be8d8c741477a567b142210304ef6aa7efd41dae
Krzysztof Adamek <kadamek@alx.pl>
2023-01-29 15:42:25 +0100

    Siatka 5x5

--- dzien_10/pierwszy_projekt/mywidget.cpp
+++ dzien_10/pierwszy_projekt/mywidget.cpp
@@ -11,18 +11,42 @@ MyWidget::MyWidget(QWidget *parent)
     setMinimumSize(400, 300);
     resize(500, 500);
 
-    QHBoxLayout* layout = new QHBoxLayout{this};
-    layout->addWidget(new CounterButton{});
-    layout->addWidget(new CounterButton{});
-    layout->addWidget(new CounterButton{});
+    CounterButton* tab[5][5]; // hint do cz. 2
 
-    QVBoxLayout* pionowy = new QVBoxLayout{};
-    layout->addLayout(pionowy);
-    pionowy->addWidget(new CounterButton{});
-    pionowy->addWidget(new CounterButton{});
+    QVBoxLayout* pionowy = new QVBoxLayout{this};
+    for (int wiersz = 0; wiersz < 5; wiersz += 1) {
+        QHBoxLayout* poziomy = new QHBoxLayout{};
+        pionowy->addLayout(poziomy);
+        for (int kolumna = 0; kolumna < 5; kolumna += 1) {
+            CounterButton* btn = new CounterButton{};
+            poziomy->addWidget(btn);
+            tab[wiersz][kolumna] = btn; // zapamietujemy adres przycisku
+        }
+    }
+    QPushButton* reset = new QPushButton{"RESET"};
+    pionowy->addWidget(reset);
+
+    //connect(tab[2][2], &QPushButton::clicked, tab[0][0], &CounterButton::przyciskWcisniety);
+    for (int w = 0; w < 5; w += 1) {
+        for (int k = 0; k < 5; k += 1) {
+            if (w > 0)
+                connect(tab[w][k], &QPushButton::clicked, tab[w - 1][k], &CounterButton::przyciskWcisniety);
+            if (k > 0)
+                connect(tab[w][k], &QPushButton::clicked, tab[w][k - 1], &CounterButton::przyciskWcisniety);
+            if (w + 1 < 5)
+                connect(tab[w][k], &QPushButton::clicked, tab[w + 1][k], &CounterButton::przyciskWcisniety);
+            if (k + 1 < 5)
+                connect(tab[w][k], &QPushButton::clicked, tab[w][k + 1], &CounterButton::przyciskWcisniety);
+        }
+    }
+
 }
 
 MyWidget::~MyWidget()
 {
 }
 
+//1. Ułóż layout 5 x 5 złożony z CounterButtonów
+
+//2. Połącz CounterButtony tak, aby wciśnięcie przycisku zwiększało
+// również liczniki jego sąsiadów (góra, dół, lewo, prawo)
